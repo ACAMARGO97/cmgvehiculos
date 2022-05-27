bf9f30f6a48410675e4a981261b33879a7bd190f
ACAMARGO97 <amarcelacamargo@uniguajira.edu.co>
2022-05-27 02:24:51 -0500

    consulta

--- apps/ventas/urls.py
+++ apps/ventas/urls.py
@@ -8,9 +8,21 @@ urlpatterns = [
     path('actualizar/<int:id_venta>/', ventaEdit, name='ventaEdit'), 
     path('eliminar/<int:id_venta>/', ventaElim, name='ventaElim'), 
 
+
     path('VehiculoVenta/', listVehiculoVentas, name='listVehiculoVentas'),
     path('nuevo_vv/', vehiculoVentasCreate, name='vehiculoVentasCreate'), 
     path('actualizar_vv/<int:id_vehiculoVenta>/', vehiculoVentasEdit, name='vehiculoVentasEdit'), 
     path('eliminar_vv/<int:id_vehiculoVenta>/', vehiculoVentasEliminar, name='vehiculoVentasEliminar'), 
 
+
+    path('Usuarios/', listUsuarios, name='listUsuarios'),
+    path('nuevo_u/', usuariosCreate, name='usuariosCreate'), 
+    path('actualizar_u/<int:id_usuario>/', usuariosEdit, name='usuariosEdit'), 
+    path('eliminar_u/<int:id_usuario>/', usuariosEliminar, name='usuariosEliminar'),
+
+
+    path('consulta1/', consulta1, name='consulta1'),
+    path('consulta2/', consulta2, name='consulta2'),
+    path('consulta3/', consulta3, name='consulta3'),
+    path('consulta4/', consulta4, name='consulta4'),
 ]
--- apps/ventas/views.py
+++ apps/ventas/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import redirect, render
-from apps.ventas.models import VehiculoVenta, Venta
+from apps.ventas.models import VehiculoVenta, Venta, Usuario
 from apps.ventas.formVenta import VentaForm
 from apps.ventas.formVehiculoVenta import VehiculoVentaForm
+from apps.ventas.formUsuario import UsuarioForm
+from django.db.models import Sum
 
 def listVentas(request):
     ventas = Venta.objects.all().order_by('id') 
@@ -84,3 +86,81 @@ def vehiculoVentasEliminar(request, id_vehiculoVenta):
        vehiculoVentas.delete()
        return redirect('ventas:listVehiculoVentas')
     return render(request,'vehiculoVentas/vehiculoVentasEliminar.html', {'vehiculoVentas': vehiculoVentas})
+
+
+
+#usuarios
+def listUsuarios(request):
+    usuarios = Usuario.objects.all().order_by('-id') 
+    context = {'ventas':usuarios} 
+    return render(request, 'usuarios/listUsuarios.html', context) 
+
+
+def usuariosCreate(request):
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST) 
+        if form.is_valid():
+            form.save()
+        return redirect('ventas:listUsuarios')
+    else:
+        form =UsuarioForm()
+        return render(request,'usuarios/usuario_form.html', {'form': form})
+
+def usuariosEdit(request, id_usuario):
+    usuario = Usuario.objects.get(pk=id_usuario)
+
+    if request.method == 'GET':
+        form = UsuarioForm(instance=usuario)
+    else:
+        form =UsuarioForm(request.POST, instance=usuario) 
+        if form.is_valid():
+            form.save()
+        return redirect('ventas:listUsuarios') 
+
+    return render(request,'usuarios/usuario_form.html', {'form': form})
+ 
+def usuariosEliminar(request, id_usuario):
+    usuario = Usuario.objects.get(pk=id_usuario)
+
+    if request.method == 'POST':
+       usuario.delete()
+       return redirect('ventas:listUsuarios')
+    return render(request,'usuarios/usuarioEliminar.html', {'usuarios': usuario})
+
+#consultas
+def consulta1(request):
+    fecha1 = request.POST.get('fecha1')
+    fecha2 = request.POST.get('fecha2')
+    consulta1=Venta.objects.filter(fecha__range=[fecha1,fecha2]).aggregate(Sum('ValorTotal'))
+    
+    context = {
+        'fecha1': fecha1,
+        'fecha2': fecha2,
+        'consulta1': consulta1,
+    }
+    print(consulta1)
+    return render(request,'consultas/consulta1.html',{'context':context})
+          
+    
+def consulta2(request):
+    fecha1 = request.POST.get('fecha1')
+    fecha2 = request.POST.get('fecha2')
+  
+    consulta2=Venta.objects.values('fecha').filter(fecha__range=[fecha1,fecha2]).annotate(total=Sum('ValorTotal'))
+    
+    return render(request,'consultas/consulta2.html',{'consulta2':consulta2})
+
+def consulta3(request):
+    fecha1 = request.POST.get('fecha1')
+    fecha2 = request.POST.get('fecha2')
+  
+    consulta3=Venta.objects.values('tipoPago').filter(fecha__range=[fecha1,fecha2]).annotate(total=Sum('ValorTotal'))
+    
+    return render(request,'consultas/consulta3.html',{'consulta3':consulta3})
+
+def consulta4(request):
+    fecha1 = request.POST.get('fecha1')
+    fecha2 = request.POST.get('fecha2')
+  
+    consulta4=Venta.objects.values('user__nombres').filter(fecha__range=[fecha1,fecha2]).annotate(total=Sum('ValorTotal'))
+    return render(request,'consultas/consulta4.html',{'consulta4':consulta4})
